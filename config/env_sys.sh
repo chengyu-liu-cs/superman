57aeda4d310796e674b8750b0d9bf0b2c98cb8a5
chengyu-liu-cs <chengyu.liu.cs@gmail.com>
2018-04-30 09:52:34 +0300

    Update env_sys.sh

--- config/env_sys.sh
+++ config/env_sys.sh
@@ -13,5 +13,5 @@ sudo install virtualenv
 VirtEnv=.virtenv
 virtualenv "$VirtEnv"
 source "$VirtEnv/bin/activate"
-pip --disable-pip-version-check install --upgrade --requirement conf/python-requirement.txt > pip-install.log
+pip --disable-pip-version-check install --upgrade --requirement python_requirements.txt > pip-install.log
 
