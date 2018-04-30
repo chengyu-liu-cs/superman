d1918804be1befb4edb7e9956eddd07338142d06
chengyu-liu-cs <chengyu.liu.cs@gmail.com>
2018-04-30 09:36:23 +0300

    Update env_sys.sh

--- config/env_sys.sh
+++ config/env_sys.sh
@@ -3,5 +3,10 @@
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install python
+brew install python
 # Install pip
 sudo easy_install pip
+# Install VirtualEnv
+sudo install virtualenv
+
