7ad991691cf22f7c1f4368ba2e67df48b45f7ab7
chengyu-liu-cs <chengyu.liu.cs@gmail.com>
2018-08-30 09:54:42 +0300

    Create als.py

---
+++ python/recommendataion/als.py
@@ -0,0 +1,72 @@
+import implicit # The Cython library
+import numpy as np
+import pandas as pd
+import random
+import sys
+
+import scipy.sparse as sparse
+from scipy.sparse.linalg import spsolve
+from sklearn.preprocessing import MinMaxScaler
+
+from als import implicit_als
+
+#-------------------------
+# LOAD AND PREP THE DATA
+#-------------------------
+raw_data = pd.read_table('/opt/data/recommendation/lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv')
+raw_data = raw_data.drop(raw_data.columns[1], axis=1)
+raw_data.columns = ['user', 'artist', 'plays']
+
+# Drop NaN columns
+data = raw_data.dropna()
+data = data.copy()
+
+# Create a numeric user_id and artist_id column
+data['user'] = data['user'].astype("category")
+data['artist'] = data['artist'].astype("category")
+data['user_id'] = data['user'].cat.codes
+data['artist_id'] = data['artist'].cat.codes
+
+# The implicit library expects data as a item-user matrix so we
+# create two matricies, one for fitting the model (item-user)
+# and one for recommendations (user-item)
+sparse_item_user = sparse.csr_matrix((data['plays'].astype(float), (data['artist_id'], data['user_id'])))
+sparse_user_item = sparse.csr_matrix((data['plays'].astype(float), (data['user_id'], data['artist_id'])))
+
+# Initialize the als model and fit it using the sparse item-user matrix
+model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=20)
+
+# Calculate the confidence by multiplying it by our alpha value.
+alpha_val = 15
+data_conf = (sparse_item_user * alpha_val).astype('double')
+
+# Fit the model
+model.fit(data_conf)
+
+
+#---------------------
+# FIND SIMILAR ITEMS
+#---------------------
+
+# Find the 10 most similar to Jay-Z
+item_id = 1355 #Jay-Z
+n_similar = 10
+
+# Get the user and item vectors from our trained model
+user_vecs = model.user_factors
+item_vecs = model.item_factors
+
+# Calculate the vector norms
+item_norms = np.sqrt((item_vecs * item_vecs).sum(axis=1))
+
+# Calculate the similarity score, grab the top N items and
+# create a list of item-score tuples of most similar artists
+scores = item_vecs.dot(item_vecs[item_id]) / item_norms
+top_idx = np.argpartition(scores, -n_similar)[-n_similar:]
+similar = sorted(zip(top_idx, scores[top_idx] / item_norms[item_id]), key=lambda x: -x[1])
+
+# Print the names of our most similar artists
+for item in similar:
+    idx, score = item
+    print data.artist.loc[data.artist_id == idx].iloc[0]
+
